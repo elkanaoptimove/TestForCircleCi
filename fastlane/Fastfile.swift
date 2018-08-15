4141eecf77dc900fe1bdd44e4402b0d8f9266131
Elkana Orbach <optimove.develop.mobile@gmail.com>
2018-08-15 15:04:38 +0300

    iPhone 8

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -14,7 +14,7 @@ class Fastfile: LaneFile {
     }
 	func ciLane() {
         desc("Build the app")
-        runTests(workspace: "TestForCircleCi.xcworkspace", devices: ["iPhone 6s", "iPad Air"], scheme: "TestForCircleCi")
+        runTests(workspace: "TestForCircleCi.xcworkspace", devices: ["iPhone 8"], scheme: "TestForCircleCi")
 		// add actions here: https://docs.fastlane.tools/actions
 //        syncCodeSigning(gitUrl: "https://github.com/elkanaoptimove/TestForCircleCi.git", appIdentifier: ["com.optimove.sdk.TestForCircleCi"], username: "optimove.develop.mobile@gmail.com")
 //        buildIosApp(scheme: "TestForCircleCi")
